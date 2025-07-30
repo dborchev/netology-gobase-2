b0ba4b883206eadab8ef2dabcef9419a3a479672
Denis Borchev <askbow@users.noreply.github.com>
2025-07-30 19:59:47 +0200

    initial struct homework

---
+++ gobase-04-struct-interface/go.mod
@@ -0,0 +1,3 @@
+module askbow.io/go-struct
+
+go 1.24.5
---
+++ gobase-04-struct-interface/main.go
@@ -0,0 +1,36 @@
+package main
+
+import “fmt”
+
+// Структура для хранения данных о сотруднике
+type Employee struct {
+}
+
+// Интерфейс для вывода информации
+type Displayable interface {
+
+}
+
+// Реализация метода Display для Employee
+func (e Employee) Display() {
+   
+}
+
+// Функция фильтрации сотрудников по возрасту и зарплате
+func FilterEmployees(employees []Employee, minAge int, minSalary int) []Employee {
+   
+    return filtered
+}
+
+func main() {
+    // Инициализация списка сотрудников
+    employees := []Employee{
+    }
+
+// Параметры фильтрации
+    minAge :=
+    minSalary :=
+
+// Фильтрация и вывод
+    filteredEmployees :=
+}
